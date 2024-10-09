995dcb7161426358c91fa49c4183ab968964cc05
Marl0nico <marlon.nicolalde@epn.edu.ec>
2024-10-08 23:45:07 -0500

    Modificación de archivo

--- Fundamentos aplicados.js
+++ Fundamentos aplicados.js
@@ -1,14 +1,14 @@
 //Escuela Politécnica Nacional
 //variables
-const añoFundación=1869
-const fundador="Presidente Gabriel García Moreno"
-const fechaFundación="30 de agosto"
-let instituciónFuncional=true
-let númeroCarreras=29
+let chico="Fireboy"
+let chica="Watergirl"
+const tipoJuego="Aventura"
+const númeroJugadores=2
+let juegoOnline=true
 //strings
-let nombreU="Escuela Politécnica Nacional"
-let primerDirector="Juan Bautista Menten"
-let nacionalidadDirector="alemana"
-let fechaPrimer_cierre="15 de septiembre de 1876"
-let fechaReapertura="4 de junio de 1946"
-console.log(`La ${nombreU} fue fundada el ${fechaFundación} de ${añoFundación} por ${primerDirector} de nacionalidad ${nacionalidadDirector}`);
+let lugar="Templo del bosque"
+let plataformasDisponibles="Android, iOS y web"
+let númeroPartidas_jugadas="Más de 11 millones"
+let público="Juego para niños"
+console.log(`El ${público} llamado ${chico} y ${chica}: El ${lugar} es un juego de ${tipoJuego} para ${númeroJugadores} jugadores disponible en ${plataformasDisponibles}`);
+
