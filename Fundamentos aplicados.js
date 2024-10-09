f8fc484bd5a9d14da8aef872bcbdf39f99abe318
Marl0nico <marlon.nicolalde@epn.edu.ec>
2024-10-08 23:11:28 -0500

    Commit 1: Labo 3

---
+++ Fundamentos aplicados.js
@@ -0,0 +1,14 @@
+//Escuela Politécnica Nacional
+//variables
+const añoFundación=1869
+const fundador="Presidente Gabriel García Moreno"
+const fechaFundación="30 de agosto"
+let instituciónFuncional=true
+let númeroCarreras=29
+//strings
+let nombreU="Escuela Politécnica Nacional"
+let primerDirector="Juan Bautista Menten"
+let nacionalidadDirector="alemana"
+let fechaPrimer_cierre="15 de septiembre de 1876"
+let fechaReapertura="4 de junio de 1946"
+console.log(`La ${nombreU} fue fundada el ${fechaFundación} de ${añoFundación} por ${primerDirector} de nacionalidad ${nacionalidadDirector}`);
